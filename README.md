13e8f5454841e95204bc459426b145496f850b61
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2025-02-27 18:16:02 +0400

    Simple Pagination Code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pagination
+
+1. Frontend üçün
+    `npm run dev`
+2. Server üçün
+    `json-server --watch db.json --port 5174`
---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSearchParams } from "react-router-dom";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5174/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const moviesPerPage = 6;
+  const indefOfFirstMovie = (currentPage - 1) * moviesPerPage;
+  const indefOfLastMovie = currentPage * moviesPerPage;
+  const slicedMovies = movies.slice(indefOfFirstMovie, indefOfLastMovie);
+  const numberOfPages = Math.ceil(movies.length / moviesPerPage);
+
+  const handlePageChange = (newPage) => {
+    setSearchParams({ page: newPage });
+  }
+
+  window.scrollTo(0, 0);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Filmlər</h1>
+      <div className="row">
+        {slicedMovies.map((movie) => (
+          <div key={movie.id} className="col-md-4 col-lg-3 mb-4">
+            <div className="card shadow-sm">
+              <img src={movie.img} className="card-img-top" alt={movie.title} />
+              <div className="card-body text-center">
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">İl: {movie.year}</p>
+                <button className="btn btn-primary">Daha ətraflı</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="pagination d-flex justify-content-center align-items-center my-5 gap-3">
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="btn btn-primary">Previous</button>
+        <span>{currentPage} of {numberOfPages}</span>
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === numberOfPages} className="btn btn-primary">Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
